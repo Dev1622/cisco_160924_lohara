3f3dbbd2f8e43f624c99754174e3e8240aa488fb
Dev1622 <deevsurya14@gmail.com>
2024-09-16 22:37:55 +0530

    Hospital Management

---
+++ 20240916/hospital_management.py
@@ -0,0 +1,42 @@
+patients = []
+
+#1.add patient
+#2.remove patient
+
+def patient_add(patient):
+    #global salaries
+    patients.append(patient)
+   # return salaries.append(salary)
+def patient_delete(patient):
+    #global salaries
+    try:
+        patients.remove(patient)
+    except ValueError as err:
+        print('No such salary')
+   # return salaries.remove(salary)
+
+
+def menu():
+    choice = int(input('''1-add_patient
+     2-delete_patient
+     3-End
+                   
+    '''))
+    if choice == 1:
+        patient = str(input('Enter patient_name:'))
+        patient_add(patient)
+        print(patients)
+
+    elif choice == 2:
+        patient = str(input('Enter patient_no:'))
+        patient_delete(patient)
+        print(patients)
+    return choice
+
+def menus():
+    choice = menu()
+    while choice != 3:
+        choice = menu()
+    print('App Ended') 
+
+menus() 
