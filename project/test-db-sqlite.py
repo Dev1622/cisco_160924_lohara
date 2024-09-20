9b981ff0f8a754136728044e544a62cf99a4445d
Dev1622 <112717464+Dev1622@users.noreply.github.com>
2024-09-20 14:48:06 +0530

    Update test-db-sqlite.py

--- project/test-db-sqlite.py
+++ project/test-db-sqlite.py
@@ -2,10 +2,5 @@ from book_db import Book,bookTablesCreate,readBookById,createBook,deleteBook,upd
 
 bookTablesCreate()
 
-""" id = createBook(Book(title='f',notes='flask is web dev framework and python package'))
-print(f'{id} is inserted')
-id = createBook(Book(title='django',notes='django is also web dev framework and python package. It includes all the batteries.'))
-print(f'{id} is inserted') """
-
 books = readAllBooks()
 print(books)
