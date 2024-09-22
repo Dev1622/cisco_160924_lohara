6c11e8d2cbe50bdfb5ebf99a0f77c298ade06ae8
Dev1622 <112717464+Dev1622@users.noreply.github.com>
2024-09-22 15:50:45 +0530

    Delete project directory

--- project/book_main.py
+++
@@ -1,4 +0,0 @@
-from book_menu import menus
-
-#8. driver program
-menus()
--- project/test-db-sqlite.py
+++
@@ -1,6 +0,0 @@
-from book_db import Book,bookTablesCreate,readBookById,createBook,deleteBook,updateBook,readAllBooks
-
-bookTablesCreate()
-
-books = readAllBooks()
-print(books)
